26ebec7c377e2eda56e01f6e60159e875c535727
KevW <wei00033@gmail.com>
2022-01-26 18:09:17 -0500

    +

--- Earl Of March Virtual Tour/Assets/TouchController.cs
+++ Earl Of March Virtual Tour/Assets/TouchController.cs
@@ -1,29 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TouchController : MonoBehaviour
 {
     public float Speed = 5f;
     private float pitch = 0f;
     private float yaw = 0f;
-    KeyCode a;
 
     private void Start(){
         yaw = 0f;
         pitch = 0f;
-        a = KeyCode.F;
     }
 
+    //The two camera movement methods
     private void Update(){
+        //This code basically moves the camera based on the mouse being held down.
         if (Input.GetMouseButton(0))
         {
-            pitch += Speed * Input.GetAxis("Mouse Y");
+            if (!IsPointerOverUIElement())
+            {
+                pitch += Speed * Input.GetAxis("Mouse Y");
 
-            yaw += Speed * -Input.GetAxis("Mouse X");
+                yaw += Speed * -Input.GetAxis("Mouse X");
 
-            pitch = Mathf.Clamp(pitch, -90f, 90f);
-            
+                pitch = Mathf.Clamp(pitch, -90f, 90f);
+
+                //Prevants the screen to flip upside down.
+                while (yaw < 0f)
+                {
+                    yaw += 360f;
+                }
+                while (yaw >= 360f)
+                {
+                    yaw -= 360f;
+                }
+            }
+        }
+        //Code for moving the camera by using key arrows. This moves the camera right
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            yaw += Speed * 0.1f;
             while (yaw < 0f)
             {
                 yaw += 360f;
@@ -32,7 +50,61 @@ public class TouchController : MonoBehaviour
             {
                 yaw -= 360f;
             }
-            transform.eulerAngles = new Vector3(pitch, yaw, 0f);
         }
+        //Code for moving the camera by using key arrows. This moves the camera left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            yaw += -Speed * 0.1f;
+            while (yaw < 0f)
+            {
+                yaw += 360f;
+            }
+            while (yaw >= 360f)
+            {
+                yaw -= 360f;
+            }
+        }
+        //Code for moving the camera by using key arrows. This moves the camera up
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pitch += -Speed * 0.1f;
+            //Mathf clamp is just a fancy way of keeping the variable in -90f and 90f.
+            pitch = Mathf.Clamp(pitch, -90f, 90f);
+        }
+        //Code for moving the camera by using key arrows. This moves the camera down
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pitch += Speed * 0.1f;
+            pitch = Mathf.Clamp(pitch, -90f, 90f);
+        }
+        //Finally with the new pitch and yaw you can set the new camera rotation.
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+        
     }
+
+    //Ignore these 3 functions
+    public static bool IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+    public static bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int i = 0; i < eventSystemRaysastResults.Count; i++){
+            RaycastResult curRaysastResult = eventSystemRaysastResults[i];
+            if (curRaysastResult.gameObject.layer == LayerMask.NameToLayer("UI"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
+    }
+
 }
---
+++ Earl Of March Virtual Tour/Assets/UIController.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+
+//Basically controlls almost all of the UI elements.
+public class UIController : MonoBehaviour
+{
+    public GameObject enabler;
+    public InputField input;
+    public Text changer;
+    int guideortele;
+    GuideSystem guideSystem;
+    public Scrollbar scroll;
+    public Camera cam;
+
+    public AudioSource source;
+    public Scrollbar audioscroll;
+
+    public GameObject options;
+    public GameObject optiontext;
+    public float minDepth;
+    public GameObject tutorial;
+
+    public GameObject locationReached;
+
+    public GameObject StartScreen;
+
+    public GameObject movementButton;
+    public GameObject teleButton;
+
+    public GameObject enableGuideSystemUIbutton;
+
+    public Dictionary<string, int> info = new Dictionary<string, int>();
+    public Dictionary<int, string> seinfo = new Dictionary<int, string>();
+
+    //Calls the setup function + assigsns some variables
+    void Start()
+    {
+        guideortele = 1;
+        setup();
+        guideSystem = GetComponent<GuideSystem>();
+    }
+
+    //Opens up the guidesystem UI
+    public void enableGuideSystemUIButton()
+    {
+        if (enableGuideSystemUIbutton.activeSelf)
+        {
+            enableGuideSystemUIbutton.SetActive(false);
+        }
+        else
+        {
+            enableGuideSystemUIbutton.SetActive(true);
+        }
+    }
+
+    bool fun = false;
+
+    //Change color of guidesystem UI area
+    private void FixedUpdate()
+    {
+        if(guideortele == 1)
+        {
+            movementButton.GetComponent<Image>().color = Color.green;
+            teleButton.GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            movementButton.GetComponent<Image>().color = Color.red;
+            teleButton.GetComponent<Image>().color = Color.green;
+        }
+        
+    }
+
+    //This disable the startscreen which is the first UI element you see
+    public void disableStartScreen()
+    {
+        StartScreen.SetActive(false);
+    }
+
+    //Disable the locationreached or the UI element you see when you reached the end of your path
+    public void disableLocationReached()
+    {
+        locationReached.SetActive(false);
+    }
+
+    //Closes the settings ui and opens the tutorial UI
+    public void entertutorial()
+    {
+        optiontext.SetActive(false);
+        tutorial.SetActive(true);
+    }
+    //Closes tutorial Ui and opens settings
+    public void exittutorial()
+    {
+        optiontext.SetActive(true);
+        tutorial.SetActive(false);
+    }
+
+    //Opens the option menu + little animation
+    public void openOptions()
+    {
+        Animator temp = options.GetComponent<Animator>();
+        options.SetActive(true);
+        optiontext.SetActive(true);
+        temp.SetBool("OptionOpen",true);
+    }
+    //The reason I have a seperate function for this is because you need the function to be IEnumerator 
+    //To have WaitForSeconds. This closes the options menu
+    public void closeOptions()
+    {
+        StartCoroutine(removeoptionsmenu());
+    }
+
+    IEnumerator removeoptionsmenu()
+    {
+        Animator temp = options.GetComponent<Animator>();
+        optiontext.SetActive(false);
+        temp.SetBool("OptionOpen", false);
+        yield return new WaitForSeconds(0.5f);
+        options.SetActive(false);
+    }
+    
+    //Constantly sets the camera field of view to what you have set in the settings. It doesn't do it when the 
+    //zoom in affect is working though.
+    //Also sets the volume according to what is in the settings
+    void Update()
+    {
+        if(Movement.test == false){
+            cam.fieldOfView = Mathf.Max(scroll.value * 100, minDepth);
+        }
+        source.volume = audioscroll.value;
+    }
+
+    //This basically gets all the information from the assets part of the unity.
+    //Basically the scripts are not connected with the assets and this function of code does it
+    void setup()
+    {
+        string path = "Assets/GraphInfo/GuideChangeInformation.txt";
+        StreamReader reader = new StreamReader(path, true);
+        string temp = reader.ReadToEnd();
+        string[] lines = temp.Split("\n"[0]);
+        foreach (string line in lines)
+        {
+            info[line.Substring(0, 4)] = int.Parse(line.Substring(5, 3));
+            seinfo[int.Parse(line.Substring(5, 3))] = line.Substring(0, 4);
+        }
+    }
+    //Controls the small little UI button in the bottem left corner of the screen.
+    public void onButtonPush()
+    {
+        if (enabler.activeSelf)
+        {
+            enabler.SetActive(false);
+            changer.text = "+";
+        }
+        else
+        {
+            enabler.SetActive(true);
+            changer.text = "-";
+        }
+    }
+
+    //Sets the user to wanting to get a path from the guide system
+    public void setGuide(){
+        guideortele = 1;
+    }
+    //Sets the user to wanting to immediately go to a location from the guide system
+    public void setTeleport()
+    {
+        guideortele = 0;
+    }
+
+    //This activates the guide system.
+    //If you are set to finding a path, it will call the path
+    //If you are sest to teleporting, you will immediately reach the location
+    //But first it gotta check if the place is a real location, then check whether you are already at the locaiton
+    //Then it calls the functions
+    public void enterButtonPush(){
+        if(info.ContainsKey(input.text)){
+            if (guideortele == 1){
+                if(RenderSettings.skybox != guideSystem.a[info[input.text]]){
+                    guideSystem.findpath(RenderSettings.skybox, guideSystem.a[info[input.text]]);
+                }else{
+                    input.text = "Error:Same location";
+                }
+            }else{
+                if (RenderSettings.skybox != guideSystem.a[info[input.text]]){
+                    guideSystem.teleportsystem(guideSystem.a[info[input.text]]);
+                }else{
+                    input.text = "Error:Same location";
+                }
+            }
+        }else{
+            input.text = "Error:Incorrect input";
+        }
+    }
+
+    //If you click exitapplication it will exit you out of the application. Wow
+    public void exitApplication()
+    {
+        Application.Quit();
+    }
+}
